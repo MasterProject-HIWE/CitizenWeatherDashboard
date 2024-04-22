ae87c13dd7ef3159431ab4bbd94c7fd45f79e35b
skaplesh <91467842+skaplesh@users.noreply.github.com>
2024-04-22 17:00:24 +0530

    import
    import

--- src/components/TableOfContent/index.jsx
+++ src/components/TableOfContent/index.jsx
@@ -2,11 +2,33 @@ import React, { useEffect, useState } from 'react';
 import TableOfDetails from './tableofdetails.json';
 import { useButtonContext } from '../ButtonContext/ButtonContext';
 import { useUserContext } from '../UserContext/UserContext';
+import { useKantonContext } from '../KantonContext/KantonContext';
+import { AgGridReact } from 'ag-grid-react';
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
 
 const WeatherDataTable = () => {
-    const [filteredData, setFilteredData] = useState(TableOfDetails);
+    const [ filteredData, setFilteredData] = useState(TableOfDetails);
     const { selectedButton } = useButtonContext();
     const { selectedUserName } = useUserContext();
+    const { selectedKantonName } = useKantonContext();
+    const [ tableData, setTableData ] = useState({columnDefs:[],rowData:[]});
+
+
+    useEffect(()=>{
+        const ndata = {
+            columnDefs: [
+             {headerName: 'UserName', field: 'username',width:100},
+             {headerName: 'Municipality', field: 'municipality',filter: true,width:100},
+             {headerName: 'Cantons', field: 'cantons',width:100},
+             {headerName: 'Severity', field: 'severity' ,filter: "agSetColumnFilter",width:100},        
+             {headerName: 'Start Time', field: 'starttime' ,filter: "agSetColumnFilter",width:100},        
+             {headerName: 'End Time', field: 'endtime' , filter: "agSetColumnFilter",width:100},           
+             {headerName: 'Post', field: 'posts' , filter: "agSetColumnFilter",width:100},           
+         ],
+         rowData:filteredData}        
+        setTableData(ndata);
+    },[filteredData,setTableData]);
 
     useEffect(() => {
         if (selectedButton === 'Total' && selectedUserName === 'noUser') {
@@ -27,40 +49,23 @@ const WeatherDataTable = () => {
         }
     }, [selectedButton, selectedUserName]);
 
+    useEffect(()=>{
+        console.log(selectedKantonName);
+        if (selectedKantonName){
+            const newData = TableOfDetails.filter(item => item.cantons.toUpperCase() === selectedKantonName.toUpperCase());
+            setFilteredData(newData);
+        }    
+    },[selectedKantonName]);
+
     return (
         <>
             <div style={{padding: '4px'}}>
                 <h1 className='cell-header'>User data</h1>
-                <table id="table-of-details">
-                    <thead>
-                        <tr style={{ position: 'sticky', top: 0 }}>
-                            {/* <th>SL</th> */}
-                            <th>Username</th>
-                            {/* <th>Cantons</th> */}
-                            <th>Municipality</th>
-                            <th>Severity</th>
-                            {/* <th>Timestamp</th> */}
-                            <th>Starttime</th>
-                            <th>Endtime</th>
-                            <th>Post</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {filteredData.map((item) => (
-                            <tr key={item.sl}>
-                                {/* <td>{item.sl}</td> */}
-                                <td>{item.username}</td>
-                                {/* <td>{item.cantons}</td> */}
-                                <td>{item.municipality}</td>
-                                <td>{item.severity}</td>
-                                {/* <td>{item.timestamp}</td> */}
-                                <td>{item.starttime}</td>
-                                <td>{item.endtime}</td>
-                                <td className="truncate-text" style={{ textAlign: 'left', }}>{item.posts}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
+                <div id="table-of-details" style={{overflow:"hidden !important",backgroundColor:"#fff",width:"400px", height:"500px"}}>
+                  <div className="ag-theme-alpine" style={{overflow:"hidden", width:"100%", height: "100%" }}>
+                     <AgGridReact  headerClass="header" gridOptions={{ rowHeight: 15,headerHeight: 30,suppressResize: true }} style={{ width:"100%", height: "100%" }} rowData={tableData.rowData} columnDefs={tableData.columnDefs} suppressMenuHide ={true}/>
+                  </div>
+ >              </div>                
             </div>
         </>
     );
